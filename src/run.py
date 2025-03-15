e1ba39a47d6325aabe8cf8f75150ed611ff40871
Pawel Kowalik <2063440-pawel-kow@users.noreply.gitlab.com>
2025-03-15 14:05:23 +0700

    Default 501 for not existing implementation

--- src/run.py
+++ src/run.py
@@ -1,4 +1,4 @@
 from app import app
 from connexion.resolver import RelativeResolver
 
-app.add_api("openapi.yaml", resolver=RelativeResolver('controller'))
+app.add_api("openapi.yaml", resolver=RelativeResolver('controller'), resolver_error=501)
