2f5ab8b87706d42191443f76dc67856d21d7b268
Pawel Kowalik <2063440-pawel-kow@users.noreply.gitlab.com>
2025-03-16 21:13:09 +0700

    Handle exceptions in domain creation and return appropriate ProblemException responses

--- src/controller/domains.py
+++ src/controller/domains.py
@@ -1,4 +1,4 @@
-from connexion import request
+from connexion import request, ProblemException
 from eppclient.epp_domains import *
 from models import *
 from rpp_to_model_mapper import *
@@ -8,20 +8,25 @@ def domains_Check(body):
     return {}, 500
 
 def domains_Create(body):
-    domain = rpp_to_domain(body)
-    
-    # Check if Expect header is set, if so return 100-continue
-    if request.headers.get('Expect') == '100-continue':
-        return None, 100
-    
-    # Call the eppclient function to create the domain
-    domainresp = epp_domains_Create(domain)
-    #HACK: this should be rather done with domain information from the response
-    domain.update(domainresp.domain)
-    
-    # Convert the response to JSON
-    response = domain_to_rpp(domain)
-    
+    try:
+        domain = rpp_to_domain(body)
+    except Exception as e:
+        raise ProblemException(status=400, title="Bad Request", detail=str(e))
+
+    try:
+        # Check if Expect header is set, if so return 100-continue
+        if request.headers.get('Expect') == '100-continue':
+            return None, 100
+        
+        # Call the eppclient function to create the domain
+        domainresp = epp_domains_Create(domain)
+        #HACK: this should be rather done with domain info reponse
+        domain.update(domainresp.domain)
+        
+        # Convert the response to JSON
+        response = domain_to_rpp(domain)
+    except Exception as e:
+        raise ProblemException(status=500, title="Internal Server Error", detail=str(e))
     return response, 201
 
 def domains_Delete(id):
